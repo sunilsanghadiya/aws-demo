3d35d07f3f0ecafbc92dfdb4257ba912117966be
sunilsanghadiya <sunilsanghadiya.code@gmail.com>
2024-10-26 11:44:56 +0530

    some change

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aws_demo.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        public HomeController() { }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Welcome to aws demo");
+        }
+    }
+}
